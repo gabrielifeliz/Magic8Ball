6ac32b06b60f4fe6ec0a7624b095cc25cf11aba3
gabrielifeliz <gfeliz3@montgomerycollege.edu>
2018-06-01 15:24:18 -0400

    updates

--- src/com/company/Magic8Ball.java
+++ src/com/company/Magic8Ball.java
@@ -13,74 +13,18 @@ public class Magic8Ball {
         output("HELLO THERE! Ask a question to the MAGIC 8-BALL...");
         String question = keyboard.nextLine();
 
-        int answerOption = 1 + rnd.nextInt(20);
-        String magic8BallAnswer = "";
+        String[] magic8BallAnswers = {"It is certain", "It is decidedly so",
+            "Without a doubt", "Yes definitely", "You may rely on it",
+            "As I see it, yes", "Most likely", "Outlook good", "Yes",
+            "Signs point to yes", "Reply hazy try again", "Ask again later",
+            "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+            "Don't count on it", "My reply is no", "My sources say no",
+            "Outlook not so good", "Very doubtful"};
 
-        switch (answerOption) {
-            case 1:
-                magic8BallAnswer = "It is certain";
-                break;
-            case 2:
-                magic8BallAnswer = "It is decidedly so";
-                break;
-            case 3:
-                magic8BallAnswer = "Without a doubt";
-                break;
-            case 4:
-                magic8BallAnswer = "Yes definitely";
-                break;
-            case 5:
-                magic8BallAnswer = "You may rely on it";
-                break;
-            case 6:
-                magic8BallAnswer = "As I see it, yes";
-                break;
-            case 7:
-                magic8BallAnswer = "Most likely";
-                break;
-            case 8:
-                magic8BallAnswer = "Outlook good";
-                break;
-            case 9:
-                magic8BallAnswer = "Yes";
-                break;
-            case 10:
-                magic8BallAnswer = "Signs point to yes";
-                break;
-            case 11:
-                magic8BallAnswer = "Reply hazy try again";
-                break;
-            case 12:
-                magic8BallAnswer = "Ask again later";
-                break;
-            case 13:
-                magic8BallAnswer = "Better not tell you now";
-                break;
-            case 14:
-                magic8BallAnswer = "Cannot predict now";
-                break;
-            case 15:
-                magic8BallAnswer = "Concentrate and ask again";
-                break;
-            case 16:
-                magic8BallAnswer = "Don't count on it";
-                break;
-            case 17:
-                magic8BallAnswer = "My reply is no";
-                break;
-            case 18:
-                magic8BallAnswer = "My sources say no";
-                break;
-            case 19:
-                magic8BallAnswer = "Outlook not so good";
-                break;
-            case 20:
-                magic8BallAnswer = "Very doubtful";
-        }
 
         output("");
         output("YOU ASKED: " + question);
-        output("MAGIC 8-BALL SAYS: " + magic8BallAnswer);
+        output("MAGIC 8-BALL SAYS: " + magic8BallAnswers[0 + rnd.nextInt(19)]);
         output("Do you have another question for the Magic 8-Ball? (y/n)");
         String choice = keyboard.next();
 
@@ -91,13 +35,13 @@ public class Magic8Ball {
             question = keyboard.nextLine();
 
             output("YOU ASKED: " + question);
-            output("MAGIC 8-BALL SAYS: " + magic8BallAnswer);
+            output("MAGIC 8-BALL SAYS: " + magic8BallAnswers[0 + rnd.nextInt(19)]);
             output("Do you have another question for the Magic 8-Ball? (y/n)");
             choice = keyboard.next();
         }
 
 
-        output("\nGOOD BYE! See you next time");
+        output("\nSee you next time. GOOD BYE!");
     }
 
     public static void output(String message) {
